024a86cfaecb5ffde615879d50de04d362cfc771
lemonchik09 <153959701+lemonchik09@users.noreply.github.com>
2024-03-30 00:42:58 +0600

    Add files via upload
    меня вдохновлял один из учеников которых Вы хвалили очень сильно я признаю что я бывает у него немного скопировал и из-за этого прошу прощения из-за того что я немного не понимал А уже было слишком поздно звать ментора

---
+++ dz_4/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import UserRegisterPage from './pages/userRegisterPage/UserRegisterPage';
+import UsersListPage from './pages/usersListPage/UsersListPage';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route index element={<UserRegisterPage />} />
+        <Route path={'/users'} element={<UsersListPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
